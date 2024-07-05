0778c010d4fd4567852f3e9d4b00ddb344cd5ced
MatouSakura <1134979484@qq.com>
2024-07-05 17:13:17 +0800

    create

---
+++ src/main/java/com/example/anpselenium/AnpSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.example.anpselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpSeleniumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnpSeleniumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anpselenium/AnpSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anpselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnpSeleniumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
